6fd224d7c3d4233dba14a90fbff4119611856821
nastacyya <15saveika@gmail.com>
2025-05-03 16:51:16 +0300

    Created header
    + added translation function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,24 +1,32 @@
-
+// 
 fetch('/assets/partials/header.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('header_div').innerHTML = data;
 });
 
-async function changeLanguage(lang) {
-    await rememberSetLang(lang);
-    
+// When page is loaded
+window.addEventListener('DOMContentLoaded', async () => {
+    const userPreferredLanguage = localStorage.getItem('language') || 'lv';
+    const langData = await fetchLanguageData(userPreferredLanguage);
+    updateContent(langData);
+});
+
+// Onclick on language buttons
+function setLanguagePreference(lang) {
+    localStorage.setItem('language', lang);
+    location.reload();
+}
+
+async function setLanguage(lang) {
+    await setLanguagePreference(lang);
     const langData = await fetchLanguageData(lang);
     updateContent(langData);
 }
 
-function rememberSetLang(lang) {
-    localStorage.setItem("language", lang);
-    location.reload();
-}
-
-function fetchLanguageData(lang) {
-
+async function fetchLanguageData(lang) {
+    const response = await fetch(`/assets/locales/${lang}.json`);
+    return response.json();
 }
 
 function updateContent(data) {
